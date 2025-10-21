fa2c7f0cfb0792478d90844fdc7d12e83816ee58
Shahnas-P <shahanasp9@gmail.com>
2025-10-21 21:40:18 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require('express')
 
 const app = express()
 
-app.use('/hello' ,(req,res)=>{
-    res.send("Server response to /hello request")
+app.get('/user/:userId',(req,res)=>{
+    console.log(req.params.userId);
+    
+        res.send("It's worked !!!")
 })
-app.use('/test',(req,res)=>{
-    res.send("Server response is here , hope you happy...")
-})
+
+
+
+
 app.listen(3000 , ()=>{
     console.log("Server is listening in port 3000");
     
