ab0b004e3ae82d7d327a63d03d3dceb6b9d28b90
Shahnas-P <shahanasp9@gmail.com>
2025-10-23 07:37:59 +0530

    Middlewares and how it work flow

---
+++ homeWork.md
@@ -0,0 +1,6 @@
+### Studies Topics 
+ - Indpendent routes 
+ - Middleware chain => route Handler  (other router hanlder without response we can call it as middleware , means function before reaching to the res of route hanlder  is middlware funciton)
+ - What is Middelware ?
+ - How express Js basically  handles requests behind the scens 
+ - app.use() and app.all() methods
--- src/app.js
+++ src/app.js
@@ -2,15 +2,29 @@ const express = require('express')
 
 const app = express()
 
-app.get('/user/:userId',(req,res)=>{
-    console.log(req.params.userId);
-    
-        res.send("It's worked !!!")
+app.use(express.json());
+
+app.use('/',(req,res,next)=>{
+   console.log("Goes through app.use()");
+   
+   // res.send("This is the app.use() response ")
+   next()
 })
 
+app.post('/admin/getAllData',(req,res)=>{
+   const token = req.body?.token
+   console.log((token));
+   
+   if(token === "xyz"){
+         res.send("Fetched All Data Successfully")
+   }else{
+      res.status(401).send("Unauthorized Error")
+   }
+})
 
-
-
+app.get('/admin/deleteUser',(req,res)=>{
+   res.send("User Deleted Successfully")
+})
 app.listen(3000 , ()=>{
     console.log("Server is listening in port 3000");
     
