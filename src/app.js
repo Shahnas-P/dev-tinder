d076478148eca8713f862f56b3ab8653e06aa8f6
Shahnas-P <shahanasp9@gmail.com>
2025-10-27 07:28:46 +0530

    feat(): Validation to schema

--- src/app.js
+++ src/app.js
@@ -9,11 +9,13 @@ app.use(express.json())
 app.post('/signup', async (req,res)=>{
     
     const user =  new User(req.body)
+
     try{
     await user.save()
     res.status(200).send("User created successfully !!")
     }catch(error){
-        res.status(400).send("Error: Unable to create user.",error.message)
+        res.status(400).json({error:error.message})
+        console.log(error?.errorResponse?.errmsg);
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,19 +2,46 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     firstName :{
-        type : String
+        type : String,
+        required:true,
+        minLength:3,
+        maxLength:20
     },
     lastName:{
-        type:String
+        type:String,
+        maxLength:20
+
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        maxLength:50,
+        trim:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender is not valid")
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"This is a default about"
+    },
+    photoUrl:{
+        type:String,
+        default:"https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-2210.jpg?semt=ais_hybrid&w=740&q=80"
+    },
+    skill:{
+        type:[String],
     }
 })
 
